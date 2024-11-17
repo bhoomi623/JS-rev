e1ec6c7e4a79d8e7481985c4fa8e38129197a15f
bhoomi623 <120084156+bhoomi623@users.noreply.github.com>
2024-11-17 13:51:05 +0000

    operators

--- 0_1Basics/03_conversionOperation.js
+++ 0_1Basics/03_conversionOperation.js
@@ -22,7 +22,7 @@ let isloggedin = ""
 
 
 let booleanisloggedin= Boolean( isloggedin)
-console.log(booleanisloggedin)   
+//console.log(booleanisloggedin)   
 
 // 1=>true ; 0=>false ; 
 // ""=>false
@@ -32,5 +32,35 @@ console.log(booleanisloggedin)
 let somenumber= 33
 
 let stringnumber =  String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber)
+//console.log(stringnumber);
+//console.log(typeof stringnumber)
+
+
+//***************************************operations*************************************** */
+
+let value =3
+let negvalue = -value
+//console.log(negvalue);  // gives -3
+
+let str1="hello"
+let str2="bhoomi"
+
+let str3= str1 + str2
+console.log(str3);
+
+//console.log("1"+2);  //12
+//console.log(1+"2");  //12
+//console.log("1"+2+2);  //122
+//console.log(1+2+"2");//32
+
+//console.log(+true)  // 1
+//console.log(+"");  //0
+
+
+let num1,numm2,num3;
+
+num1=num2=num3=2+2
+
+let gamecounter = 100
+gamecounter++;  // ++gamecounter will also give 101   its about prefix and postfix
+console.log(gamecounter);
