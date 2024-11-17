c2518d79e44e2028afdc6b93cdd9724b2d8c591e
bhoomi623 <120084156+bhoomi623@users.noreply.github.com>
2024-11-17 13:24:44 +0000

    conversion of datatypes

---
+++ 0_1Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score ="33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+
+
+//for a value of score like 33ac it will give NaN means not a number
+//for a value of score like null it will give 0 
+
+// "33"=> 33
+//"33abc"=> NaN
+// true=>1; false =>0
+
+
+let isloggedin = ""
+
+
+let booleanisloggedin= Boolean( isloggedin)
+console.log(booleanisloggedin)   
+
+// 1=>true ; 0=>false ; 
+// ""=>false
+//"asdf"=> true
+
+
+let somenumber= 33
+
+let stringnumber =  String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
