74eb538a26f82cd7caef7067fd51d5fe5b7d56b4
bhoomi623 <120084156+bhoomi623@users.noreply.github.com>
2024-11-17 10:57:59 +0000

    datatypes

---
+++ 0_1Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"// treat all js code as a newer version
+
+//alert(3+3)  //we are using nodejs not browser
+
+console.log(3+
+    
+    3) // code readability should be high
+
+console.log("bhoomi")  
+
+let name = "bhoomi"
+let age = 18
+let isloggedin= false
+let state=null // vo khali h
+// number=> 2 to power 53
+//bigint
+//string=> ""
+//bool => true,false
+//null=> standalone value
+//undefined =>
+// symbol=> unique
+
+
+//object
+
+console.log(typeof "bhoomi")  //string
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
